366ab6049bf2b16b9fd257a3b574e47a2133ded0
Raul Lugo <lugo@sipgate.de>
2019-07-12 07:50:32 +0200

    feat: finish decimal to roman function

--- roman_numerals/decimal_to_roman.py
+++ roman_numerals/decimal_to_roman.py
@@ -1,24 +1,37 @@
-roman_dictionary = {
-    1: 'I',
-    4: 'IV',
-    5: 'V',
-    9: 'IX',
-    10: 'X',
-    40: 'XL',
-    50: 'L',
-    90: 'XC',
-    100: 'C',
-    400: 'CD',
-    500: 'D',
-    900: 'CM',
-    1000: 'M'
-}
+from collections import OrderedDict
+
+roman_dictionary = OrderedDict([
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I'),
+])
 
 
-def is_direct_value(decimal):
-    return decimal in roman_dictionary
+def decimal_to_roman(input_decimal):
+    roman_string = ''
+
+    while input_decimal != 0:
+        for decimal, roman in roman_dictionary.items():
+            if input_decimal in roman_dictionary:
+                roman_string += roman_dictionary.get(input_decimal)
+                input_decimal = 0
+            if input_decimal > decimal:
+                times_to_repeat = int(input_decimal / decimal)
+                roman_string += (times_to_repeat * roman)
+                input_decimal -= times_to_repeat * decimal
+                continue
+    return roman_string
 
 
-def receive_decimal():
-    decimal_int = int(input('Introduce valid decimal number (int) '))
-    return decimal_int
+if __name__ == '__main__':
+    print(decimal_to_roman(44))
--- tests/test_decimal_to_roman.py
+++ tests/test_decimal_to_roman.py
@@ -1,17 +1,38 @@
 import unittest
-import mock
-from roman_numerals.decimal_to_roman import *
+from roman_numerals.decimal_to_roman import decimal_to_roman
 
 
 class UnitTests(unittest.TestCase):
 
-    def test_is_direct_value(self):
-        self.assertTrue(is_direct_value(1))
-        self.assertFalse(is_direct_value(2))
+    def test_direct_values(self):
+        self.assertEqual('I', decimal_to_roman(1))
+        self.assertEqual('IV', decimal_to_roman(4))
+        self.assertEqual('V', decimal_to_roman(5))
+        self.assertEqual('IX', decimal_to_roman(9))
+        self.assertEqual('X', decimal_to_roman(10))
+        self.assertEqual('XL', decimal_to_roman(40))
+        self.assertEqual('L', decimal_to_roman(50))
+        self.assertEqual('XC', decimal_to_roman(90))
+        self.assertEqual('C', decimal_to_roman(100))
+        self.assertEqual('CD', decimal_to_roman(400))
+        self.assertEqual('D', decimal_to_roman(500))
+        self.assertEqual('CM', decimal_to_roman(900))
+        self.assertEqual('M', decimal_to_roman(1000))
 
-    def test_input_decimal(self):
-        with mock.patch('builtins.input', return_value="1"):
-            self.assertEquals(receive_decimal(), 1)
+    def test_repeated_values(self):
+        self.assertEqual('II', decimal_to_roman(2))
+        self.assertEqual('III', decimal_to_roman(3))
+        self.assertEqual('XX', decimal_to_roman(20))
+        self.assertEqual('XXX', decimal_to_roman(30))
+        self.assertEqual('CC', decimal_to_roman(200))
+        self.assertEqual('CCC', decimal_to_roman(300))
+        self.assertEqual('MM', decimal_to_roman(2000))
+        self.assertEqual('MMM', decimal_to_roman(3000))
+
+    def test_do_not_repeat_more_than_three(self):
+        self.assertEqual('XLIX', decimal_to_roman(49))
+        self.assertEqual('XLIV', decimal_to_roman(44))
+        self.assertEqual('CDXLIV', decimal_to_roman(444))
 
 
 if __name__ == '__main__':
